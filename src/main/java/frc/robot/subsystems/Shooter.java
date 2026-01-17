d8d21e7debb04caf4ea41b7c98cee286e1a20676
1250-programming <114453660+1250-programming@users.noreply.github.com>
2026-01-17 17:58:27 -0500

    Update Shooter.java
    I added multiple  TalonFX velocity control and position control
    
    Co-Authored-By: sebastian1250 <255587907+sebastian1250@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,10 +1,27 @@
 package frc.robot.subsystems;
 
+import java.lang.module.Configuration;
+import java.security.PrivateKey;
+
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+
 import edu.wpi.first.epilogue.Logged;
 
 @Logged
 public class Shooter {
-    public Shooter() {
-
+    
+    TalonFX lodercamTalonFX;
+     VelocityVoltage lodercamvelocitycontrol = new VelocityVoltage(0).withSlot(0);
+     PositionVoltage lodercamPositionVoltage = new PositionVoltage(0);
+public Shooter() {
     }
+    TalonFX leader;
+     VelocityVoltage leadervelocityVoltage = new VelocityVoltage(0).withSlot(0);
+     TalonFX follower;
+       VelocityVoltage  followervVelocityVoltage = new VelocityVoltage(0).withSlot(0);
+      TalonFX leaderflywheel;
+      
 }
