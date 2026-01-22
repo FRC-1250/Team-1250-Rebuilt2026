4638dc281d3ee476a13c111599a67877aa4b4397
1250-programming <114453660+1250-programming@users.noreply.github.com>
2026-01-22 17:43:35 -0500

    Update Shooter.java
    added precursor fly wheel leader/follower
    velocity control for both
    added fly wheel leader/follower
    velocity control for both
    and added hood solenoid

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -9,19 +9,26 @@ import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
 
 @Logged
 public class Shooter {
-    
-    TalonFX lodercamTalonFX;
-     VelocityVoltage lodercamvelocitycontrol = new VelocityVoltage(0).withSlot(0);
-     PositionVoltage lodercamPositionVoltage = new PositionVoltage(0);
-public Shooter() {
+
+    TalonFX poderCamTalonFX;
+    VelocityVoltage loderCamVelocityControl = new VelocityVoltage(0).withSlot(0);
+    PositionVoltage loderCamPositionVoltage = new PositionVoltage(0);
+
+    TalonFX precursorFlyWheelLeader;
+    VelocityVoltage precursorFlyWheelVelocityVoltage = new VelocityVoltage(0).withSlot(0);
+    TalonFX precursorFlyWheelFollower;
+
+    TalonFX flyWheelLeader;
+    VelocityVoltage flyWheelVelocityVoltage = new VelocityVoltage(0).withSlot(0);
+    TalonFX flyWheelFollower;
+
+    Solenoid hoodSolenoid = new Solenoid(PneumaticsModuleType.REVPH, 0);
+
+    public Shooter() {
     }
-    TalonFX leader;
-     VelocityVoltage leadervelocityVoltage = new VelocityVoltage(0).withSlot(0);
-     TalonFX follower;
-       VelocityVoltage  followervVelocityVoltage = new VelocityVoltage(0).withSlot(0);
-      TalonFX leaderflywheel;
-      
 }
