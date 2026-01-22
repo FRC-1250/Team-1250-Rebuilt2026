b4a0ee6489e88bc960e6f3b00332a482c186fd6d
1250-programming <114453660+1250-programming@users.noreply.github.com>
2026-01-22 18:43:38 -0500

    Update Shooter.java
    added motor out put configs brake mode and inverted value clockwise

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,12 +1,11 @@
 package frc.robot.subsystems;
 
-import java.lang.module.Configuration;
-import java.security.PrivateKey;
-
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
@@ -15,7 +14,7 @@ import edu.wpi.first.wpilibj.Solenoid;
 @Logged
 public class Shooter {
 
-    TalonFX poderCamTalonFX;
+    TalonFX loderCamTalonFX;
     VelocityVoltage loderCamVelocityControl = new VelocityVoltage(0).withSlot(0);
     PositionVoltage loderCamPositionVoltage = new PositionVoltage(0);
 
@@ -30,5 +29,8 @@ public class Shooter {
     Solenoid hoodSolenoid = new Solenoid(PneumaticsModuleType.REVPH, 0);
 
     public Shooter() {
+        MotorOutputConfigs motorOutputConfigs = new MotorOutputConfigs();
+        motorOutputConfigs.NeutralMode = NeutralModeValue.Brake;
+        motorOutputConfigs.Inverted = InvertedValue.Clockwise_Positive;
     }
 }
