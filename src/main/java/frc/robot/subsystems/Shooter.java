200ab912047639391f38b005f4e21c62f8e5e4a5
1250-programming <114453660+1250-programming@users.noreply.github.com>
2026-01-24 17:53:06 -0500

    Update Shooter.java
    added loder cam position and velocity control slot0 and 1  configs

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,6 +1,12 @@
 package frc.robot.subsystems;
 
+import static edu.wpi.first.units.Units.Hertz;
+import static edu.wpi.first.units.Units.Volts;
+
 import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.Slot1Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
@@ -8,6 +14,7 @@ import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.units.measure.Frequency;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
 
@@ -28,9 +35,52 @@ public class Shooter {
 
     Solenoid hoodSolenoid = new Solenoid(PneumaticsModuleType.REVPH, 0);
 
+    void setloderCamPosition(double rotations) {
+        loderCamTalonFX.setControl(
+                loderCamPositionVoltage
+                        .withPosition(rotations)
+                        .withFeedForward(Volts.of(0)));
+    }
+
+    void setloderCamVelocity(double rotations) {
+        loderCamTalonFX.setControl(
+                loderCamPositionVoltage
+                        .withVelocity(rotations)
+                        .withFeedForward(Volts.of(0)));
+    }
+
+    @Logged(name = "Position")
+    public double getRotations() {
+        return loderCamTalonFX.getPosition().getValueAsDouble();
+    }
+
+    @Logged(name = "velocity")
+    public double loderCamPositionVoltage() {
+        return loderCamTalonFX.getVelocity().getValueAsDouble();
+    }
+
     public Shooter() {
         MotorOutputConfigs motorOutputConfigs = new MotorOutputConfigs();
         motorOutputConfigs.NeutralMode = NeutralModeValue.Brake;
         motorOutputConfigs.Inverted = InvertedValue.Clockwise_Positive;
+        loderCamTalonFX.getPosition().setUpdateFrequency(Frequency.ofBaseUnits(200, Hertz));
+        loderCamTalonFX.getVelocity().setUpdateFrequency(Frequency.ofBaseUnits(200, Hertz));
+        Slot0Configs intakePositionPIDConfigs = new Slot0Configs()
+                .withKP(0)
+                .withKI(0)
+                .withKD(0);
+
+        Slot1Configs intakeVelocityPIDConfigs = new Slot1Configs()
+                .withKS(0)
+                .withKV(0)
+                .withKP(0)
+                .withKI(0)
+                .withKD(0);
+        TalonFXConfiguration intakeTalonFXConfiguration = new TalonFXConfiguration();
+        intakeTalonFXConfiguration.Slot0 = intakePositionPIDConfigs;
+        intakeTalonFXConfiguration.Slot1 = intakeVelocityPIDConfigs;
+        intakeTalonFXConfiguration.CurrentLimits.SupplyCurrentLimit = 30;
+        intakeTalonFXConfiguration.CurrentLimits.SupplyCurrentLimitEnable = true;
+
     }
 }
