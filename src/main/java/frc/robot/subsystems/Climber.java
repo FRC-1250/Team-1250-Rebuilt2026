0d2a9e8f601fae853af72d5118df1c9702c6a83e
1250-programming <114453660+1250-programming@users.noreply.github.com>
2026-02-05 18:58:01 -0500

    started climber position control functions
    I started on the climber and added position controls but didn't start the commands.
    
    Co-Authored-By: kcpop <196865645+kcpop@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -1,11 +1,48 @@
 package frc.robot.subsystems;
 
+import static edu.wpi.first.units.Units.Rotations;
+import static edu.wpi.first.units.Units.Volts;
+
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.controls.TorqueCurrentFOC;
+import com.ctre.phoenix6.hardware.TalonFX;
+import frc.robot.subsystems.Climber;
 import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 @Logged
 public class Climber {
 
-    public Climber() {
+    public enum LeftClimberPosition {
+        HOOKOUT(0),
+        TOWERL1(0),
+        TOWERL2(0),
+        HOME(0);
 
+        public final double rotations;
+
+        LeftClimberPosition(double rotations) {
+            this.rotations = rotations;
+        }
     }
+
+    private static final PositionVoltage climberPositionControl = new PositionVoltage(0);
+
+    private TalonFX LeftClimber = new TalonFX(50);
+    private TalonFX RightClimber = new TalonFX(51);
+
+    public Climber() {
+    }
+
+    public boolean isLeftClimberNearPosition(double position) {
+        return isLeftClimberNearPosition(position);
+    }
+
+    private void setLeftClimberPosition(double rotations) {
+        LeftClimber.setControl(
+                climberPositionControl
+                        .withPosition(rotations)
+                        .withFeedForward(Volts.of(0)));
+    }
+
 }
