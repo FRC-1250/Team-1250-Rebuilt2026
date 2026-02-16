6021a942e4b60ac7477142c7ead83f57275bf170
Rodney Lewis <rodneylewis95@gmail.com>
2026-02-16 08:20:19 -0500

    Update Climber.java
    - Cleanup climber and use leader/follower syntax
    - Implement monitored subsystem logic

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -1,19 +1,23 @@
 package frc.robot.subsystems;
 
-import static edu.wpi.first.units.Units.Rotations;
 import static edu.wpi.first.units.Units.Volts;
 
+import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.PositionVoltage;
-import com.ctre.phoenix6.controls.TorqueCurrentFOC;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.MotorAlignmentValue;
+
 import frc.robot.subsystems.Climber;
+import frc.robot.telemetry.HealthMonitor;
+import frc.robot.telemetry.MonitoredSubsystem;
 import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 @Logged
-public class Climber {
+public class Climber extends SubsystemBase implements MonitoredSubsystem {
 
-    public enum LeftClimberPosition {
+    public enum ClimberPosition {
         HOOKOUT(0),
         TOWERL1(0),
         TOWERL2(0),
@@ -21,25 +25,38 @@ public class Climber {
 
         public final double rotations;
 
-        LeftClimberPosition(double rotations) {
+        ClimberPosition(double rotations) {
             this.rotations = rotations;
         }
     }
 
-    private static final PositionVoltage climberPositionControl = new PositionVoltage(0);
+    private final PositionVoltage climberPositionControl = new PositionVoltage(0);
+    private final Color systemColor = new Color(0, 0, 0);
 
-    private TalonFX LeftClimber = new TalonFX(50);
-    private TalonFX RightClimber = new TalonFX(51);
+    private TalonFX climberLeader = new TalonFX(50);
+    private TalonFX climberFollower = new TalonFX(51);
 
     public Climber() {
+        climberFollower.setControl(new Follower(climberLeader.getDeviceID(), MotorAlignmentValue.Opposed));
     }
 
-    public boolean isLeftClimberNearPosition(double position) {
-        return isLeftClimberNearPosition(position);
+    public boolean isClimberNearPosition(double position) {
+        return isClimberNearPosition(position);
     }
 
-    private void setLeftClimberPosition(double rotations) {
-        LeftClimber.setControl(
+    @Override
+    public void registerWithHealthMonitor(HealthMonitor monitor) {
+        monitor.addComponent(getSubsystem(), "Climber leader", climberLeader);
+        monitor.addComponent(getSubsystem(), "Climber follower", climberFollower);
+    }
+
+    @Override
+    public Color getSubsystemColor() {
+        return systemColor;
+    }
+
+    public void setClimberPosition(double rotations) {
+        climberLeader.setControl(
                 climberPositionControl
                         .withPosition(rotations)
                         .withFeedForward(Volts.of(0)));
