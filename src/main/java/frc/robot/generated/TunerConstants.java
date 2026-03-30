48952c668958fef25c6010b2994d62ee652ed65a
Rodney Lewis <rodneylewis95@gmail.com>
2026-03-30 11:50:09 -0400

    Update TunerConstants.java

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -200,7 +200,7 @@ public class TunerConstants {
      */
     public static CommandSwerveDrivetrain createDrivetrain() {
         return new CommandSwerveDrivetrain(
-                DrivetrainConstants, FrontLeft, FrontRight, BackLeft, BackRight);
+                DrivetrainConstants, 200, FrontLeft, FrontRight, BackLeft, BackRight);
     }
 
     /**
